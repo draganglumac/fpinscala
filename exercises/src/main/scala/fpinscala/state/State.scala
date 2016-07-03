d7f840efc0ff9bee4dca03d19d60b018bd1248d9
Dragan Glumac <dragan.glumac@gmail.com>
2016-07-03 11:20:09 +0100

    Exercises for Chapter 6. Unhappy with the last one, I'll have to study State a lot more to understand it fully.

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -168,9 +168,34 @@ object State {
   } yield ()
 
   sealed trait Input
+
   case object Coin extends Input
+
   case object Turn extends Input
+
   case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  // Not happy with my understanding of State :-( Hopefully I'll get it
+  // from later chapters like IO monad.
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    // case class State[S, +A](run: S => (A, S)) { ... }
+    // S - Machine
+    // A - (Int, Int)
+
+    def update = (i: Input) => (s: Machine) =>
+      (i, s) match {
+        case (_, Machine(_, 0, _)) => s
+        case (Coin, Machine(false, _, _)) => s
+        case (Turn, Machine(true, _, _)) => s
+        case (Coin, Machine(true, candy, coin)) =>
+          Machine(false, candy, coin + 1)
+        case (Turn, Machine(false, candy, coin)) =>
+          Machine(true, candy - 1, coin)
+      }
+
+    for {
+      _ <- sequence(inputs map (modify[Machine] _ compose update))
+      s <- get
+    } yield (s.coins, s.candies)
+  }
 }
