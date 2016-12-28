0767c1cb7043143c6796d943c3b1cf39a54db376
Dragan Glumac <dragan.glumac@gmail.com>
2016-12-28 15:44:16 +0000

    Exercises up to 6.9 completed.

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -127,6 +127,16 @@ object RNG {
       if (i + (n - 1) - mod >= 0) unit(mod)
       else nonNegativeLessThan(n)
     }
+
+  def _map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
+
+  def map2ViaMixed[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b))) // the same as _map2 by definition of _map
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
